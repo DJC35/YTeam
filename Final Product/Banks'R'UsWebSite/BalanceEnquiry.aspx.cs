0843cd6e1e5d608882aba70d87c5009dc09c3494
DJC35 <jarekalen@gmail.com>
2017-03-24 15:16:33 -0400

    Update BalanceEnquiry.aspx.cs

--- Final Product/Banks'R'UsWebSite/BalanceEnquiry.aspx.cs
+++ Final Product/Banks'R'UsWebSite/BalanceEnquiry.aspx.cs
@@ -1,12 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
-public partial class BalanceEnquiry : System.Web.UI.Page
+public partial class BalanceEnquiry : Page
 {
+
+    SqlConnection dbConnection = new SqlConnection("Data Source=stusql;Initial Catalog=ITP262_Banks_R_Us ;Integrated Security=true");
+    int accountNumber;
+    int accountBalance;
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -22,13 +28,35 @@ public partial class BalanceEnquiry : System.Web.UI.Page
         Response.Redirect(AccountDDL.SelectedValue);
     }
 
-    protected void StatementDDL_SelectedIndexChanged(object sender, EventArgs e)
+    protected void StatmentDDL_SelectedIndexChanged(object sender, EventArgs e)
     {
-        Response.Redirect(StatementDDL.SelectedValue);
+        Response.Redirect(StatmentDDL.SelectedValue);
     }
 
-    protected void LogoutButton_Click(object sender, EventArgs e)
+    protected void Submit_Click(object sender, EventArgs e)
     {
-        Response.Redirect("Login.aspx");
+        accountNumber = int.Parse(AccountNumber.Text);
+        SqlCommand insert = new SqlCommand("SELECT ACCOUNT_BALANCE FROM ACCOUNT WHERE ACCOUNT_ID = " + accountNumber + ");",  dbConnection);
+        try
+        {
+            dbConnection.Open();
+            SqlDataReader rd = insert.ExecuteReader();
+            if (rd.HasRows)
+            {
+                rd.Read(); // read first row
+                accountBalance = rd.GetInt32(0);
+            }
+            dbConnection.Close();
+        }
+        catch (SqlException sqla)
+        {
+            Response.Write(sqla.Message);
+        }
+        Output.Text = "Account Balance : " + accountBalance;
+    }
+    protected void Reset_Click(object sender, EventArgs e)
+    {
+        AccountNumber.Text = "";
+        Output.Text = "";
     }
 }
